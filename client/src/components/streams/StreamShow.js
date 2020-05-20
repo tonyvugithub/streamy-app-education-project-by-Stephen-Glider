edd0655f1aff2162190de11a288538ecdca583a3
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2020-05-20 18:29:13 -0400

    add rtmpserver

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../store/actions';
 
@@ -9,16 +10,52 @@ const StreamShow = ({
   },
   stream,
 }) => {
+  //create a ref to refer to the video component
+  const videoRef = useRef(null);
+  const player = useRef(null);
+
   useEffect(() => {
     fetchStream(id);
+    buildPlayer();
   }, []);
 
+  //Call build player if the stream is successfully fetched/updated
+  //The return function is for cleaning up the player after we navigate to
+  //different page aka StreamShow component unmount
+  useEffect(() => {
+    buildPlayer();
+    return () => {
+      if (player.current) {
+        player.current.destroy();
+      }
+    };
+  }, [stream]);
+
+  //Clean up the player when component unmount
+
+  //This function is to tell React that we only attach
+  //the video player if we have a stream info has been fetched
+  const buildPlayer = () => {
+    //If the player is already exist or stream is not fetched then do nothing
+    if (player.current || !stream) {
+      return;
+    }
+    player.current = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    player.current.attachMediaElement(videoRef.current);
+    player.current.load();
+  };
+
+  //Loading component
   if (!stream) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
       <h1>{stream.title}</h1>
       <h5>{stream.description}</h5>
     </div>
@@ -30,4 +67,5 @@ const mapStateToShow = (state, ownProps) => {
     stream: state.streams[ownProps.match.params.id],
   };
 };
+
 export default connect(mapStateToShow, { fetchStream })(StreamShow);
